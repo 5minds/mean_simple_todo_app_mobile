97de65efb41fe1b9bd8a7a751c7e8b62110863e4
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2014-08-24 16:24:03 +0200

    fixed layout

--- www/js/services/task.js
+++ www/js/services/task.js
@@ -4,6 +4,8 @@ angular.module('simpleToDoApp').factory('Task', function($resource, AppConfig) {
 
   var host = AppConfig.API_HOST || 'http://localhost:3000/';
 
+  console.log(host);
+
   return $resource(host + 'tasks/:id', 
           {id: '@_id'},
           {'update': { 'method': 'PUT'}}
